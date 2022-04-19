142bef59caf10a84997422d007cf966b1afbdbd9
AlanWu888 <74883696+AlanWu888@users.noreply.github.com>
2022-04-19 10:59:21 +0100

    added custom and preset skate games

---
+++ app/src/main/java/com/example/goskate/PresetGameActivity.java
@@ -0,0 +1,44 @@
+package com.example.goskate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PresetGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preset_game);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        // set selected
+        bottomNavigationView.setSelectedItemId(R.id.Dice);
+        // perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.Dice:
+                        return true;
+
+                    case R.id.Map:
+                        startActivity(new Intent(getApplicationContext(), MapActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Profile:
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
