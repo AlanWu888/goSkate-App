6ff6f5e454f17c0684e470407e3626214bc30207
AlanWu888 <74883696+AlanWu888@users.noreply.github.com>
2022-04-20 12:27:06 +0100

    working add tricks fragment

--- app/src/main/java/com/example/goskate/CustomTricksFragment.java
+++ app/src/main/java/com/example/goskate/CustomTricksFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class CustomTricksFragment extends Fragment {
 
@@ -22,18 +24,27 @@ public class CustomTricksFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_custom_tricks, container, false);
 
-        String[] menuItems = {"Ollie", "Nollie", "FrontSide 180"};
+        ArrayList<String> trickNames = new ArrayList<String>();
 
+        EditText edit_trickName = (EditText)view.findViewById(R.id.edit_trickName);
         ListView listView = (ListView)view.findViewById(R.id.listView_tricks);
+        Button btn_addTrick = (Button)view.findViewById(R.id.btn_addTrick);
 
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menuItems
+                getActivity(), android.R.layout.simple_list_item_1, trickNames
         );
 
         listView.setAdapter(listViewAdapter);
 
+        btn_addTrick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                trickNames.add(edit_trickName.getText().toString());
+                listViewAdapter.notifyDataSetChanged();
+
+                edit_trickName.setText("");
+            }
+        });
 
         return view;
     }
